f2718332c5b670f2175ea8db32c2410e1527de52
Rimsha Khalid <bihrimsha@bih.local>
2025-09-29 12:13:51 +0500

    code

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from './components/layout';
 import { ThemeProvider } from './context/theme-provider';
+import WeatherDashboard from './pages/weather-dashboard';
+import CityPage from './pages/city-page';
 
 function App() {
 
@@ -10,7 +12,8 @@ function App() {
       <ThemeProvider defaultTheme='dark'>
         <Layout>
           <Routes>
-            <Route path='/'  />
+            <Route path='/' element={<WeatherDashboard />} />
+            <Route path='/city/:cityName' element={<CityPage />} />
           </Routes>
         </Layout>
       </ThemeProvider>

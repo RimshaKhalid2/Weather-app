9b2ba541c7d9742e1a5a871cb51fb11d55c594af
Rimsha Khalid <bihrimsha@bih.local>
2025-10-02 11:49:18 +0500

    code / files

--- src/components/city-search.tsx
+++ src/components/city-search.tsx
@@ -1,18 +1,40 @@
 import { useState } from "react";
 import { Button } from "./ui/button"
 import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from "./ui/command"
-import { Loader2, Search } from "lucide-react";
+import { Clock, Loader2, Search, Star, XCircle } from "lucide-react";
 import { useLocationSearch } from "@/hooks/use-weather";
+import { useNavigate } from "react-router-dom";
+import { useSearchHistory } from "@/hooks/use-search-history";
+import { format } from "date-fns";
+import { useFavorite } from "@/hooks/use-favorite";
 
 const CitySearch = () => {
     const [open, setOpen] = useState(false);
     const [query, setQuery] = useState("");
+    const navigate = useNavigate();
 
     const {data: locations , isLoading} = useLocationSearch(query)
+    const { history, clearHistory, addToHistory } = useSearchHistory();
 
-    const handleSelect = () => {
+    const handleSelect = (cityData: string) => {
+        const [lat, lon, name, country] = cityData.split("|");
+        
+        // Add to search history
 
-    }
+        addToHistory.mutate({
+            query,
+            name,
+            lat: parseFloat(lat),
+            lon: parseFloat(lon),
+            country,
+            searchedAt: 0
+        });
+
+        setOpen(false);
+        navigate(`/city/${name}?lat=${lat}&lon=${lon}`);
+    };
+
+    const { favorites } = useFavorite();
 
   return (
     <>
@@ -35,15 +57,76 @@ const CitySearch = () => {
         {query.length > 2 && !isLoading && (
             <CommandEmpty>No cities found.</CommandEmpty>
         )}
-        <CommandGroup heading="Favorites">
-          <CommandItem>Calendar</CommandItem>
-        </CommandGroup>
 
-        <CommandSeparator />
+        {favorites.length > 0 && (
+                <CommandGroup heading="Favorites">
+                    {favorites.map((location) => {
+                        return (
+                            <CommandItem 
+                            key={location.id}
+                            value={`${location.lat}|${location.lon}|${location.name}|${location.country}`}
+                            onSelect={handleSelect}
+                        >
+                            <Star className="mr-2 h-4 w-4 text-yellow-500" />
+                            <span>{location.name}</span>
+                            {location.state && (
+                                <span className="text-sm text-muted-foreground">
+                                    , {location.state}
+                                </span>
+                            )}
+                            <span className="text-sm text-muted-foreground">
+                                , {location.country}
+                            </span>
+                        </CommandItem>
+                        )
+                    })}
+                </CommandGroup>
+        )}
 
-        <CommandGroup heading="Recent searches">
-          <CommandItem>Calendar</CommandItem>
-        </CommandGroup>
+                {history.length > 0 && (
+            <>
+                <CommandSeparator />
+                <CommandGroup>
+                    <div className="flex items-center justify-between px-2 my-2">
+                        <p className="text-sm text-muted-foreground">
+                            Recent Searches
+                        </p>
+                        <Button
+                            variant="ghost"
+                            size="sm"
+                            onClick={() => clearHistory.mutate()}
+                        >
+                            <XCircle className="h-4 w-4" />
+                            Clear
+                        </Button>
+                    </div>
+
+                    {history.map((location) => {
+                        return (
+                            <CommandItem 
+                            key={`${location.lat}-${location.lon}`}
+                            value={`${location.lat}|${location.lon}|${location.name}|${location.country}`}
+                            onSelect={handleSelect}
+                        >
+                            <Clock className="mr-2 h-4 w-4 text-muted-foreground" />
+                            <span>{location.name}</span>
+                            {location.state && (
+                                <span className="text-sm text-muted-foreground">
+                                    , {location.state}
+                                </span>
+                            )}
+                            <span className="text-sm text-muted-foreground">
+                                , {location.country}
+                            </span>
+                            <span className="ml-auto text-sm text-muted-foreground">
+                                {format(location.searchedAt, "MMM d, h:mm a")}
+                            </span>
+                        </CommandItem>
+                        )
+                    })}
+                </CommandGroup>
+            </>
+        )}
 
         <CommandSeparator />
 
--- src/hooks/use-search-history.ts
+++ src/hooks/use-search-history.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useLocalStorage } from "./use-local-storage";
 
 interface SearchHistoryItem{
@@ -18,6 +18,8 @@ export function useSearchHistory() {
         []
     );
 
+    const queryClient = useQueryClient();
+
     const historyQuery = useQuery({
         queryKey: ["search-history"],
         queryFn: () => history,
@@ -33,6 +35,34 @@ export function useSearchHistory() {
                 id: `${search.lat}-${search.lon}-${Date.now()}`,
                 searchedAt: Date.now(),
             };
+
+            const filteredHistory = history.filter (
+                (item) => !(item.lat === search.lat && item.lon === search.lon)
+            );
+
+            const newHistory = [newSearch, ...filteredHistory].slice(0, 10);
+            
+            setHistory(newHistory);
+            return newHistory;
+        },
+        onSuccess:(newHistory) => {
+            queryClient.setQueryData(["search-history"], newHistory);
         },
     });
+
+    const clearHistory = useMutation({
+        mutationFn: async() => {
+            setHistory([]);
+            return [];
+        },
+        onSuccess:() => {
+            queryClient.setQueryData(["search-history"], []);
+        },
+    });
+
+    return {
+        history: historyQuery.data??[],
+        addToHistory,
+        clearHistory,
+    }
 }
